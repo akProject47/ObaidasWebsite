f4e6147f5ea42b99898b8b14853d6ece24888d05
PhilikusHD <philip.dammann06@gmail.com>
2024-10-16 14:51:54 +0200

    Fixed: Deprecation warning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const App = () => <h1>Hello</h1>
+const App = () => <h1>Hello!</h1>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
